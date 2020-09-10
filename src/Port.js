18456c0871940bbedb45d1571836f62085ba456e
Andy Harrison <andyhcodes@gmail.com>
2020-09-10 13:43:44 +0100

    made DRY

--- src/Port.js
+++ src/Port.js
@@ -1,6 +1,18 @@
 class Port {
   constructor(name) {
     this.name = name;
+    this.ships = [];
+  }
+
+  addShip(shipName) {
+    this.ships.push(shipName);
+  }
+
+  removeShip(shipName) {
+    const arrayWithoutShip = this.ships.filter((ship) => {
+      return ship !== shipName;
+    });
+    this.ships = arrayWithoutShip;
   }
 }
 
--- tests/Port.test.js
+++ tests/Port.test.js
@@ -1,16 +1,38 @@
 const Ship = require("../src/Ship.js");
 const Port = require("../src/Port.js");
 
-describe("port", () => {
+describe("port and ships", () => {
+  let port;
+  let ship;
+
+  beforeEach(() => {
+    port = new Port("Calais");
+    ship = "titanic";
+  });
+
   it("returns an object", () => {
-    const port = new Port("Calais");
     expect(new Port("Calais")).toBeInstanceOf(Object);
   });
-});
 
-describe("port name", () => {
   it("returns the port name", () => {
-    const port = new Port("Calais");
     expect(port.name).toEqual("Calais");
+  });
+
+  it("can add a ship", () => {
+    port.addShip(ship);
+
+    expect(port.ships).toContain(ship);
+  });
+
+  it("removes a ship", () => {
+    const ship1 = "QE2";
+
+    port.addShip(ship);
+    port.addShip(ship1);
+
+    port.removeShip(ship1);
+
+    expect(port.ships).not.toContain(ship1);
+    expect(port.ships.length).toBe(1);
   });
 });
